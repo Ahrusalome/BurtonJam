738c9c39e1d64586d269c80e0d1da1b7d891b0f9
Ahrusalome <90481871+Ahrusalome@users.noreply.github.com>
2024-03-10 16:17:06 +0100

    spider integrated

--- Assets/Scripts/SpiderStopper.cs
+++ Assets/Scripts/SpiderStopper.cs
@@ -7,18 +7,28 @@ public class SpiderStopper : MonoBehaviour
     [SerializeField] private float stepBackSpeed;
     [SerializeField] private Transform stepBackFinishPoint;
     [SerializeField] private PlayerController playerController;
+    private bool spiderIsTriggered = true;
+    [SerializeField] private Spider spider;
     private void OnTriggerEnter(Collider other)
     {
-        if (!GameManager._manager.spiderIsBig)
-            return;
-        StartCoroutine(StepBack(other.transform));
+        if (GameManager._manager.spiderIsBig)
+        {
+            spider.changeMultiplier = 2f;
+            StartCoroutine(spider.ChangeScale());
+            StartCoroutine(StepBack(other.transform));
+        }
+        else
+        {
+            spider.changeMultiplier = 0.2f;
+            StartCoroutine(spider.ChangeScale());
+        }
     }
 
     private IEnumerator StepBack(Transform playerTransform)
     {
         for (float elapsedTime = 0; elapsedTime <= 1; elapsedTime += Time.unscaledDeltaTime)
         {
-            playerController.HandleHeadBob();
+            spiderIsTriggered = false;
             yield return playerTransform.position = new Vector3(transform.position.x, 16.9f, Mathf.Lerp(transform.position.z, stepBackFinishPoint.position.z, elapsedTime*stepBackSpeed));
         }
     }
