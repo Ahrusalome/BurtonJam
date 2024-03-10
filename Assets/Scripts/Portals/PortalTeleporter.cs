17c644b5a6afae7e41281a2088c5af58e514a399
lepetoc <lepetoctgp@outlook.fr>
2024-03-10 12:10:22 +0100

    Teleporters are finally working

---
+++ Assets/Scripts/Portals/PortalTeleporter.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour {
+
+	public Transform player;
+    public CharacterController characterController;
+	public Transform reciever;
+
+	private bool playerIsOverlapping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            // If this is true: The player has moved across the portal
+            if (dotProduct < 0f)
+            {
+                // Teleport him!
+                float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+
+                characterController.enabled = false;
+                player.position = reciever.position + positionOffset;
+                characterController.enabled = true;
+                Debug.Log(player.position);
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = false;
+		}
+	}
+}
