4e97f0066cc6720ecae9705e3f0f0af9f493cb95
Ahrusalome <90481871+Ahrusalome@users.noreply.github.com>
2024-03-10 14:38:39 +0100

    phone integrated

---
+++ Assets/Scripts/PhoneInterract.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PhoneInterract : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private GameObject phoneCanvas;
+        public void Interract()
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            phoneCanvas.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+        }
+        
+    }
+}
--- Assets/Scripts/phone.cs
+++ Assets/Scripts/phone.cs
@@ -9,18 +9,16 @@ public class phone : MonoBehaviour
     private string code;
     [SerializeField] private string password;
     [SerializeField] private TMP_Text passwordDidplay;
+
+    [SerializeField] private GameObject phoneCanvas;
+    [SerializeField] private BoxCollider phoneCollider;
     // Start is called before the first frame update
     void Start()
     {
         hits = 0;
         Debug.Log($"start");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     private void UpdatePassword()
     {
@@ -39,9 +37,15 @@ public class phone : MonoBehaviour
         UpdatePassword();
     }
 
+    public void Close()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        phoneCanvas.SetActive(false);
+        phoneCollider.enabled = true;
+    }
     public void done()
     {
-        if (code == password) 
+        if (code == password)
         {
             Debug.Log($"Unlock");
         }
